bb98b8d029226f7a18954b5f78c6ad1a81ba593b
Hollister009 <hipnos.az@gmail.com>
2019-10-13 19:00:06 +0300

    added cartIcon and cartDropdown components to the app

---
+++ src/components/cartDropdown/cartDropdown.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CustomButton from '../customButton';
+
+import './cartDropdown.scss';
+
+const CartDropdown = () => (
+  <div className="cart-dropdown">
+    <div className="cart-items" />
+    <CustomButton>GO TO CHECKOUT</CustomButton>
+  </div>
+);
+
+export default CartDropdown;
