d9545be5283775bf587198a0170257214b2b11eb
Hollister009 <hipnos.az@gmail.com>
2019-11-03 18:39:15 +0200

    added redux-persist package

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 import './index.scss';
 import App from './App';
@@ -13,7 +14,9 @@ const rootElement = document.getElementById('root');
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Router>
   </Provider>
 , rootElement);
