c0df117b5bd6291b2a1b7742321c08c1d1822f73
Hollister009 <hipnos.az@gmail.com>
2019-12-10 15:28:32 +0200

    cartDropdown, cartIcon, cartItem, checkoutItem - created styled components

---
+++ src/components/checkoutItem/checkoutItem.styles.jsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const CheckoutItemContainer = styled.div`
+  width: 100%;
+  display: flex;
+  min-height: 100px;
+  border-bottom: 1px solid darkgrey;
+  padding: 15px 0;
+  font-size: 20px;
+  align-items: center;
+
+  .name,
+  .quantity,
+  .price {
+    width: 23%;
+  }
+`;
+
+export const ImageContainer = styled.div`
+  width: 23%;
+  padding-right: 15px;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const QuantityContainer = styled.span`
+  display: flex;
+
+  .arrow {
+    cursor: pointer;
+  }
+
+  .value {
+    margin: 0 10px;
+  }
+`;
+
+export const RemoveButtonContainer = styled.div`
+  padding-left: 12px;
+  cursor: pointer;
+`;
+
